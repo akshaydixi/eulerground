27ebeccef6a47c35f4b3821259ff0d3b293a1dab
Akshay Dixit <akshaydixi@gmail.com>
2013-08-30 20:01:35 +0530

    #2 in clojure

--- eulerground-clojure/2.clj
+++ eulerground-clojure/2.clj
@@ -6,8 +6,25 @@
 (def SHI (/ (- 1 (Math/pow 5 0.5)) 2) )
 
 (defn fib [x] 
-    ( / ( - ( Math/pow PHI x ) ( Math/pow SHI x ) ) (Math/pow 5 0.5) )
+    (int ( / ( - ( Math/pow PHI x ) ( Math/pow SHI x ) ) (Math/pow 5 0.5) ))
 )
 
-(
+(def fibs (loop [v (vector),
+                i 2,
+                elem (fib 1)]
 
+                (if (< elem 4000000)
+                    (recur (conj v elem)
+                           (inc i)
+                           (fib i)
+                    )
+                    
+                    (apply vector v)
+ 
+                )
+        )
+)
+
+
+                    
+(println (reduce + ( filter (fn [x] (= 0 (mod x 2))) fibs ) ) )
