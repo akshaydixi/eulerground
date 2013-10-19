fa4680f8d1ade76c5b29349d736a488a6ab87efa
Akshay Dixit <akshaydixi@gmail.com>
2013-10-19 22:25:47 +0530

    4.clj

--- eulerground-clojure/4.clj
+++ eulerground-clojure/4.clj
@@ -1,2 +1,35 @@
 ;find the largest prime factor of a composite number
+(defn isprime [n] 
+		(if (== 2 n) true
+		(let [limit (Math/sqrt n)]
+				(if (zero? (rem n 2)) false 
+				(loop [i 3]
+					(if (<= i limit)
+						(if (zero? (rem n i)) false (recur (+ i 2)) )
+						true
+						)
+					)
+				)
+			)
+		)
+	)
 
+(defn divisors [n]
+	(let [divs []]
+		(loop [i 2 divs [1,n]]
+			(if (zero? (rem n i))
+				(do (recur (+ i 1) (conj divs i)))
+				(if (<= i (/ n 2))
+					(recur (+ 1 i) divs)
+					(reverse divs)
+					)
+				)
+			)
+		)
+	)
+
+(def number 600951475143)
+(println (divisors number))
+(println (first (filter isprime (divisors number))))
+
+	
